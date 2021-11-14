52f9772183a94394e7ddbf6740d5a691edb9b93d
CristianDavid <cristianescobarcdec@gmail.com>
2021-11-14 18:32:36 -0500

    crud facturas

--- Backend/index.js
+++ Backend/index.js
@@ -3,24 +3,66 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const db = require('./src/db/crud_facturas');
+
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Servidos De Sastreria Min tic')
 });
 
+//Trae todas las facturas
 app.get('/facturas', (req, res) => {
     db.getFacturas( function(facturas) {
         res.send(facturas); 
     });
 });
 
+//Trae las facturas con un id especifico
+app.get('/factura/:id', (req, res) => {
+    const id = req.params.id;
+    db.getOneFactura(id, function(factura) {
+        res.json(factura); 
+    });   
+});
+
+//Crea una Factura
+app.post('/add-factura', (req, res) => {
+    const factura = req.body;
+    db.addFactura(factura, function(mensaje) {
+        res.send(mensaje); 
+    });
+});
+
+//Actualiza una Factura sobreescribiendola
+app.put('/actualizar-factura/:id', (req, res) => {
+    const id = req.params.id;
+    const factura = req.body;
+    db.actualizarFactura(id, factura, function(mensaje) {
+        res.send(mensaje); 
+    });
+});
+
+//Modifica la factura, de manera parcial
+app.patch('/modificar-factura/:id', (req, res) => {
+    const id = req.params.id;
+    const factura = req.body;
+    db.modificarFactura(id, factura, function(mensaje) {
+        res.send(mensaje); 
+    });
+});
+
+//Elimina una factura
+app.delete('/eliminar-factura/:id', (req, res) => {
+    const id = req.params.id;
+    db.eliminarFactura(id, function(mensaje) {
+        res.send(mensaje); 
+    });
+});
 
 app.listen(port, () => {
     console.log('Server is running on port', port);
 });
 
 
-const db = require('./src/db/crud_facturas');
-
-db.getFacturas(); 
--- Backend/src/db/crud_facturas.js
+++ Backend/src/db/crud_facturas.js
@@ -1,5 +1,6 @@
 const db = require('./firebase');
 
+//Obtiene todas las facturas
 function getFacturas(callback) {
     return db.collection('Facturas').get()
     .then( refDoc => {
@@ -11,10 +12,70 @@ function getFacturas(callback) {
     })
     .catch( err => {
         //console.error(`Error en las facturas ${err}`);
-        callback(`Error to get Experts ${err}`);
+        callback(`Error Al traer las Facturas ${err}`);
     });
 }
 
+//obtiene una factura por id
+function getOneFactura (idFactura, callback) {
+    return db.collection('Facturas').doc(idFactura).get()
+    .then( doc => {
+        callback(doc.data());
+    })
+    .catch( err => {
+        callback(`Error al traer la factura ${err}`);
+    });
+}
+
+//Crea una factura
+function addFactura (factura, callback) {
+    return db.collection('Facturas').add(factura)
+    .then( () => {
+        callback('Factura Agregada');
+    })
+    .catch( err => {
+        callback(`Error al crear la factura ${err}`);
+    });
+}
+
+//actualizacion de factura con sobreescritura, el metodo set en firebase sobrescribe la informacion
+function actualizarFactura (id, factura, callback) {
+    return db.collection('Facturas').doc(id).set(factura)
+    .then( () => {
+        callback('Factura Actualizada');
+    })
+    .catch( err => {
+        callback(`Error al actualizar la factura ${err}`);
+    });
+}
+
+//Realiza una modificacion a una factura, actualizacion parcial no sobreescribe los datos
+function modificarFactura (id, factura, callback) {
+    return db.collection('Facturas').doc(id).update(factura)
+    .then( () => {
+        callback('Factura Modificada');
+    })
+    .catch( err => {
+        callback(`Error al Modificar la factura ${err}`);
+    });
+}
+
+function eliminarFactura (id, callback) {
+    return db.collection('Facturas').doc(id).delete()
+    .then( () => {
+        callback('Factura Eliminada');
+    })
+    .catch( err => {
+        callback(`Error al eliminar la factura ${err}`);
+    });
+}
+
+
 module.exports = {
-    getFacturas
+    getFacturas,
+    getOneFactura,
+    addFactura,
+    actualizarFactura,
+    modificarFactura,
+    eliminarFactura
 }
