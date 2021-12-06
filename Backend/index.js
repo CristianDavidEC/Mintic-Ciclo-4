3e49880c32ad7533ab622f573aaf84987a9d6ad5
CristianDavid <cristianescobarcdec@gmail.com>
2021-12-05 22:18:25 -0500

    Heruko Config

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const APIRoutes = require('./routes/indexRoutes.js');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //app.use(express.static('public'));
 app.use(express.json());
--- frontend/src/components/Prendas/Prendas.js
+++ frontend/src/components/Prendas/Prendas.js
@@ -6,7 +6,6 @@ import { useParams } from "react-router";
 
 const Prendas = (props) => {
     const idFactura = useParams().id;
-    
     var suma = 0;
     useEffect(() => {
         props.estado.forEach(valor => {
