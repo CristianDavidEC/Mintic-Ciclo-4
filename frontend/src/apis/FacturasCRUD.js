9cf353a3e19ff7245d63058a8a1074501c4ab191
CristianDavid <cristianescobarcdec@gmail.com>
2021-12-05 22:46:28 -0500

    config firebase frontend

--- frontend/src/apis/FacturasCRUD.js
+++ frontend/src/apis/FacturasCRUD.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:5000";
+const url = "https://sastreria-bakend.herokuapp.com";
 
 export function searchFacturasEstadoFact(estadoFactura, callback) {
     axios.get(url + "/facturas/search/" + estadoFactura)
